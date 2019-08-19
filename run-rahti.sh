b8076d3a4c09b18d54d5c10b0c79ff558cf437dc
fullmetalfelix <vampirer@tiscalinet.it>
2019-08-19 15:36:39 +0300

    script

--- run-rahti.sh
+++ run-rahti.sh
@@ -7,6 +7,9 @@ mkdir -p src
 cd Mira
 
 mv config-rahti.py config.py
+mkdir models
+curl 'https://lilablobssc.blob.core.windows.net/models/camera_traps/megadetector/megadetector_v3.pb' > models/megadetector_v3.pb
+
 
 export FLASK_APP=app.py
 export FLASK_DEBUG=1
